63f033e0e7baa117d73c61abc05707f82e0adba0
Hai Huynh Ngoc <haihuynhngoc24@gmail.com>
2024-07-30 15:19:44 +0700

    chore: config build options

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import del from 'rollup-plugin-delete';
+import replace from '@rollup/plugin-replace';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   input: 'src/index.ts',
@@ -10,15 +13,21 @@ export default {
     {
       file: 'dist/index.js',
       format: 'cjs',
-      sourcemap: false,
+      sourcemap: !isProduction,
     },
     {
       file: 'dist/index.esm.js',
       format: 'esm',
-      sourcemap: false,
+      sourcemap: !isProduction,
     },
   ],
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development',
+      ),
+      preventAssignment: true,
+    }),
     del({ targets: 'dist/*' }),
     resolve(), // Resolve node_modules
     commonjs(), // Convert CommonJS modules to ES modules
